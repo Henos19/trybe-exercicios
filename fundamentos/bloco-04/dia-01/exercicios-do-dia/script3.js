8f7d6df487f2ba0548e864ff40b64f2419b10832
Henos <henosvinicius13@gmail.com>
2022-04-06 18:46:12 -0300

    código do exercício 3

---
+++ fundamentos/bloco-04/dia-01/exercicios-do-dia/script3.js
@@ -0,0 +1,13 @@
+const a = 45;
+const b = 15;
+const c = 9;
+
+if (a > b && a > c){
+    console.log("a é o maior número");
+}
+else if (b > a && b > c){
+    console.log("b é o maior número");
+}
+else {
+    console.log("c é o maior número");
+}
