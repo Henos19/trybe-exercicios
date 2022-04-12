20d5388895fde31a4f1cf5a1fd6a03d0d22befc2
Henos <henosvinicius13@gmail.com>
2022-04-12 13:29:20 -0300

    Resolução exercício 3

---
+++ fundamentos/bloco-04/dia-04/Functions/script3.js
@@ -0,0 +1,16 @@
+function min(array){
+    let minNumber = 100;
+    for(let range of array){
+        if( range < minNumber ){
+            minNumber = range;
+        }
+    }
+    return minNumber;
+}
+
+function indexMin (array){
+    console.log(array.indexOf(min(array)))
+}
+
+let list = [2, 4, 6, 7, 10, 0, -3];
+indexMin(list);
