f53e6f2e6dd03cbb940cb4066f3c00787d179d8e
Henos <henosvinicius13@gmail.com>
2022-04-12 11:45:17 -0300

    resolução do exercício 5

--- fundamentos/bloco-04/dia-04/script1.js
+++ fundamentos/bloco-04/dia-04/script1.js
@@ -16,3 +16,21 @@ console.log('');
 for (let values in info){
 	console.log(info[values]);
 }
+
+let info2 = {
+  personagem: 'Tio patinhas',
+  origem: 'Christmas on Bear Mountain, Dell"s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+console.log('');
+
+for (let values2 in info) {
+	if(values2 !== 'recorrente') {
+		console.log(info[values2] + ' e ' + info2[values2]);;
+	}
+	else {
+		console.log('Ambos recorrentes')
+	}
+}
