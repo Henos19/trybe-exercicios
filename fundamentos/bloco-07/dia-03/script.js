6af73e4135759583d26a692f36e630a1ca9cc92c
Henos <henosvinicius13@gmail.com>
2022-05-16 09:26:05 -0300

    Resolução dos exercícios 02 ao 04

--- fundamentos/bloco-07/dia-03/script.js
+++ fundamentos/bloco-07/dia-03/script.js
@@ -5,4 +5,22 @@ const sum = (a, b) => {
   return a + b;
 };
 
-module.exports = {sum};
+const myRemove = (arr, item) => {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (arr[index] !== item){
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+module.exports = {sum, myRemove, myFizzBuzz};
--- fundamentos/bloco-07/dia-03/script.test.js
+++ fundamentos/bloco-07/dia-03/script.test.js
@@ -1,4 +1,4 @@
-const {sum} = require('./script');
+const {sum, myRemove, myFizzBuzz} = require('./script');
 
 describe('Testes da função sum', () => {
   it('A soma de 4 mais 5 é 9', () => {
@@ -9,5 +9,35 @@ describe('Testes da função sum', () => {
   });
   it('a ou b não são números', () => {
     expect(() => {sum(4,'5')}).toThrow('Você não digitou um número');
+  });
+});
+
+describe('Teste da função myRemove', () => {
+  it('Verifica se myRemove([1, 2, 3, 4], 3) retorna o array [1, 2, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  });
+  it('Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+  });
+});
+
+describe('Teste da função myFizzBuzz', () => {
+  it('Verifica se um número divisível por 3 e 5 retorna o valor esperado', () => {
+    expect(myFizzBuzz(15)).toBe('fizzbuzz');
+  });
+  it('Verifica se um número divisível por 3 retorna o valor esperado', () => {
+    expect(myFizzBuzz(6)).toBe('fizz');
+  });
+  it('Verifica se um número divisível por 5 retorna o valor esperado', () => {
+    expect(myFizzBuzz(25)).toBe('buzz');
+  });
+  it('Verifica se um número que não é divisível por 3 ou 5 retorna o valor esperado', () => {
+    expect(myFizzBuzz(28)).toBe(28);
+  });
+  it('Verifica se um parâmetro não numérico retorna o valor esperado', () => {
+    expect(myFizzBuzz('15')).toBeFalsy();
   });
 });
